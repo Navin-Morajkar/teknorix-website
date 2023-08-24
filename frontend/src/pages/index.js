222e382b4cdf4477817c1daf1ee87658dc26f81c
Navin Morajkar <navinsika@gmail.com>
2023-08-24 16:33:43 +0530

    products page update

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,17 +1,6 @@
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import Header from "../components/header/header" 
-import header_image from '../components/header_image/header_image'
-import Cards from "../components/six_cards/six_cards"
-import { Container } from 'postcss' 
-import Container_left from '../components/container_left/container_left' 
-import Achievement from "../components/achievement/achievement"
-import Style from "../components/six_cards/six_cards.module.css" 
 import React from 'react'
 import useFetch from '../hooks/useFetch.js'
-
-
-// const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
 
