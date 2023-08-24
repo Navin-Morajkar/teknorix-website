b49572ef1aa480bf79f2d9034637166f09d7c4c1
Navin Morajkar <navinsika@gmail.com>
2023-08-24 15:32:41 +0530

    config_file updated

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
-
-module.exports = {
   images: {
     domains: ['res.cloudinary.com'], // Add more domains if needed
-  }, 
-  nextConfig
+  },
 }
+
+module.exports = nextConfig;
