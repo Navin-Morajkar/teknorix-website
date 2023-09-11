5d97e71e1fa5cd53625bacb9f34ac14c7c9f94d5
Sank8-bit <sankshe132002@gmail.com>
2023-09-11 16:38:51 +0530

    error fixed

--- frontend/src/pages/careers/index.js
+++ frontend/src/pages/careers/index.js
@@ -1,11 +1,11 @@
-import HeaderTest from "@/components/HeaderTest/HeaderTest";
+import Header from "@/components/Header/Header";
 import { useState, useEffect } from "react";
 
 export default function index() {
 
   const [headerData, setHeaderData] = useState([])
   useEffect(() => {
-    makeApiCall("http://13.233.214.226:1337/api/headers?populate=*&filters[page][$eq]=careersPage", setHeaderData);
+    makeApiCall("http://13.233.214.226:1337/api/headers?populate=*&filters[page][$eq]=CareersPage", setHeaderData);
     //http://13.233.214.226:1337/api/headers?populate=*&filters[page][$eq]=homePage
     //http://13.233.214.226:1337/api/headers?populate[headerImage][fields][0]=name&populate[headerImage][fields][1]=url&populate[bodyImage][fields][1]=url&filters[page][$eq]=homePage
 
@@ -30,8 +30,8 @@ export default function index() {
   return (
     <div>
       <h2>Careers Page</h2>
-      <HeaderTest data={getDataBySortOrder(0)} />
-      <HeaderTest data={getDataBySortOrder(1)} />
+      <Header data={getDataBySortOrder(0)} />
+      <Header data={getDataBySortOrder(1)} />
     </div>
   )
 }
