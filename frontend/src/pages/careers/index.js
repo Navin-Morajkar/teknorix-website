21a031e0aff291e8e3695ea68f3fc9ce865755eb
Sank8-bit <sankshe132002@gmail.com>
2023-09-11 16:41:03 +0530

    header code added

--- frontend/src/pages/careers/index.js
+++ frontend/src/pages/careers/index.js
@@ -2,17 +2,17 @@ import Header from "@/components/Header/Header";
 import { useState, useEffect } from "react";
 
 export default function index() {
-
-  const [headerData, setHeaderData] = useState([])
+  const [headerData, setHeaderData] = useState([]);
   useEffect(() => {
-    makeApiCall("http://13.233.214.226:1337/api/headers?populate=*&filters[page][$eq]=CareersPage", setHeaderData);
+    makeApiCall(
+      "http://13.233.214.226:1337/api/headers?populate=*&filters[page][$eq]=CareersPage",
+      setHeaderData
+    );
     //http://13.233.214.226:1337/api/headers?populate=*&filters[page][$eq]=homePage
     //http://13.233.214.226:1337/api/headers?populate[headerImage][fields][0]=name&populate[headerImage][fields][1]=url&populate[bodyImage][fields][1]=url&filters[page][$eq]=homePage
-
-  }, [])
+  }, []);
 
   const makeApiCall = (url, setFunction) => {
-
     fetch(url)
       .then((response) => {
         return response.json();
@@ -20,8 +20,8 @@ export default function index() {
       .then((data) => {
         //console.log("API Response:", data);
         setFunction(data.data);
-      })
-  }
+      });
+  };
 
   const getDataBySortOrder = (sortOrder) => {
     return headerData.find((item) => item.attributes.SortOrder === sortOrder);
@@ -29,9 +29,8 @@ export default function index() {
 
   return (
     <div>
-      <h2>Careers Page</h2>
       <Header data={getDataBySortOrder(0)} />
       <Header data={getDataBySortOrder(1)} />
     </div>
-  )
+  );
 }
