e26b1d54d8ef904afda9a02c2fcd4afa27d27977
IsaiahDcosta <93965458+IsaiahDcosta@users.noreply.github.com>
2023-09-12 10:10:33 +0530

    services page

--- frontend/src/pages/services/index.js
+++ frontend/src/pages/services/index.js
@@ -1,9 +1,36 @@
+
 import Header from "@/components/Header/Header";
-export default function index() {
-  return (
-    <div>
-        <h2>SERVICES</h2>
-    </div>
-  )
+import ContainerLeft from "@/components/ContainerLeft/ContainerLeft";
+
+
+export async function getServerSideProps() {
+  const headerResponse = await fetch(
+    "http://13.233.214.226:1337/api/headers?populate=*&filters[page][$eq]=ServicesPage"
+  );
+  const headerData = await headerResponse.json();
+
+  const portfolioResponse = await fetch(
+    "http://13.233.214.226:1337/api/our-works?populate[image][fields][1]=url"
+  );
+  const portfolioData = await portfolioResponse.json();
+
+  return {
+    props: {
+      headerData: headerData.data,
+      portfolioData: portfolioData.data,
+    },
+  };
 }
 
+export default function Home({ headerData, portfolioData }) {
+  const getDataBySortOrder = (data, sortOrder) => {
+    return data.find((item) => item.attributes.SortOrder === sortOrder);
+  };
+
+  return (
+    <div>
+      <Header data={getDataBySortOrder(headerData, 0)} />
+      
+    </div>
+  );
+}
