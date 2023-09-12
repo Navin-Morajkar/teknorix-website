2b82f8a1bbc852bd9758a492a645924859232ecf
Navin Morajkar <navinsika@gmail.com>
2023-09-12 13:30:21 +0530

    layout updated context API

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,8 +1,12 @@
 import React from "react";
 import Style from "../Header/Header.module.css";
 import Image from "next/image";
+import { useContext } from "react";
+import { MyContext } from "../MyContext";
 
 export default function Header({ data }) {
+  const { setHeaderImageLink } = useContext(MyContext);
+
   const host = "http://13.233.214.226:1337";
   let headerImage = null;
   let bodyImage = null;
@@ -11,20 +15,14 @@ export default function Header({ data }) {
     headerImage = data.attributes.headerImage?.data;
     bodyImage = data.attributes.bodyImage?.data;
   }
+  if (headerImage) {
+    setHeaderImageLink(host + headerImage.attributes.url);
+  }
 
   return (
     <div>
       {data ? (
         <>
-          {headerImage && (
-            <Image
-              src={host + headerImage.attributes.url}
-              alt="Header Image"
-              width="1700"
-              height="1000"
-            />
-          )}
-
           <div className={Style.header}>
             <div style={{ marginLeft: "20%", paddingLeft: "2%" }}>
               <h1>{data.attributes.title}</h1>
