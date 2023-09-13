6a27cc1d4260016e398a9bdf7a9f92b3e97ffc96
IsaiahDcosta <93965458+IsaiahDcosta@users.noreply.github.com>
2023-09-13 12:33:02 +0530

    Done with ui/ux, product strategty, technical architecture design

--- frontend/src/pages/services/it-consulting/product-strategy/index.js
+++ frontend/src/pages/services/it-consulting/product-strategy/index.js
@@ -1,8 +1,85 @@
-export default function index() {
-    return (
-      <div>
-          <h2>Product Strategy</h2>
-      </div>
-    )
-  }
+import Header from "@/components/Header/Header";
+import ContainerLeft from "@/components/ContainerLeft/ContainerLeft";
+import ContainerRight from "@/components/ContainerRight/ContainerRight";
+import Styles from "@/components/SixCards/SixCards.module.css";
+import OurWork from "@/components/OurWork/OurWork";
+import OurJobs from "@/components/OurJobs/OurJobs";
+import QuoteForm from "@/components/QuoteForm/QuoteForm";
+import Container from "@/components/Container/Container";
+
+
+export async function getServerSideProps() {
+  const headerResponse = await fetch("http://13.233.214.226:1337/api/headers?populate=*&filters[page][$eq]=ProductStrategyPage");
+
+  const serviceAdvantageResponse = await fetch("http://13.233.214.226:1337/api/service-advantages?populate=*");
+  const serviceVectorResponse = await fetch("http://13.233.214.226:1337/api/service-page-vectors?populate=*");
+
+  const headerData = await headerResponse.json();
   
+  const serviceAdvantageData = await serviceAdvantageResponse.json();
+  const serviceVectorData=await serviceVectorResponse.json();
+  
+  return {
+    props: {
+      headerData: headerData.data,
+      serviceAdvantageData:serviceAdvantageData.data,
+      serviceVectorData:serviceVectorData.data
+      
+      
+    },
+  };
+}
+
+export default function Home({ headerData, serviceAdvantageData,serviceVectorData }) {
+  
+  const getDataBySortOrder = (data, sortOrder) => {
+    return data.find((item) => item.attributes.SortOrder === sortOrder);
+  };
+
+  const filterService= (data, type, sortOrder) => {
+    return data.find(
+      (item) =>
+        item.attributes.Type === type && item.attributes.SortOrder === sortOrder
+    );
+  };
+
+  const filterImageByType = (data, type) => {
+    return data.filter((item) => item.attributes.Type === type);
+  };
+
+
+  const filteredSvg = filterImageByType(serviceVectorData, "CaterTo");
+
+  const filteredProcess = filterImageByType(serviceVectorData, "OurProcess");
+
+
+  return (
+    <div>
+
+      
+      <Header data={getDataBySortOrder(headerData, 0)} />
+      <ContainerLeft data={filterService(serviceAdvantageData,"WhyUs",1)} />
+      <div className={Styles.child}>
+        {filteredSvg.map((image) => (
+          <Container key={image.id} data={image} />
+        ))}
+      </div>
+      
+      
+      
+      <h1 className="text-center text-8xl">
+        Our Process
+      </h1>
+      <div className={Styles.child}>
+        {filteredProcess.map((image) => (
+          <Container key={image.id} data={image} />
+        ))}
+      </div>
+      <Header data={getDataBySortOrder(headerData,1)} />
+
+      
+        <QuoteForm />
+
+    </div>
+  );
+}
--- frontend/src/pages/services/it-consulting/ui-ux-design/index.js
+++ frontend/src/pages/services/it-consulting/ui-ux-design/index.js
@@ -12,22 +12,25 @@ export async function getServerSideProps() {
   const headerResponse = await fetch("http://13.233.214.226:1337/api/headers?populate=*&filters[page][$eq]=UIUXPage");
 
   const serviceAdvantageResponse = await fetch("http://13.233.214.226:1337/api/service-advantages?populate=*");
+  const serviceVectorResponse = await fetch("http://13.233.214.226:1337/api/service-page-vectors?populate=*");
 
   const headerData = await headerResponse.json();
   
   const serviceAdvantageData = await serviceAdvantageResponse.json();
+  const serviceVectorData=await serviceVectorResponse.json();
   
   return {
     props: {
       headerData: headerData.data,
-      serviceAdvantageData:serviceAdvantageData.data
+      serviceAdvantageData:serviceAdvantageData.data,
+      serviceVectorData:serviceVectorData.data
       
       
     },
   };
 }
 
-export default function Home({ headerData, serviceAdvantageData }) {
+export default function Home({ headerData, serviceAdvantageData,serviceVectorData }) {
   
   const getDataBySortOrder = (data, sortOrder) => {
     return data.find((item) => item.attributes.SortOrder === sortOrder);
@@ -40,6 +43,14 @@ export default function Home({ headerData, serviceAdvantageData }) {
     );
   };
 
+  const filterImageByType = (data, type) => {
+    return data.filter((item) => item.attributes.Type === type);
+  };
+
+  const filteredSvg = filterImageByType(serviceVectorData, "CaterTo");
+
+  const filteredTechnology = filterImageByType(serviceVectorData, "DesignTechnology");
+
 
   return (
     <div>
@@ -48,6 +59,22 @@ export default function Home({ headerData, serviceAdvantageData }) {
       <Header data={getDataBySortOrder(headerData, 0)} />
       <ContainerLeft data={filterService(serviceAdvantageData,"UI/UX",1)} />
       <Header data={getDataBySortOrder(headerData,1)} />
+
+      <div className={Styles.child}>
+        {filteredSvg.map((image) => (
+          <Container key={image.id} data={image} />
+        ))}
+      </div>
+
+      <h1 className="text-center text-6xl">
+          What technologies we use?
+      </h1>
+
+      <div className={Styles.child}>
+        {filteredTechnology.map((image) => (
+          <Container key={image.id} data={image} />
+        ))}
+      </div>
 
       
         <QuoteForm />
