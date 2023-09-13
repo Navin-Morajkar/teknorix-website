aefd10f93100213d277d5600078fe3fe4baba4cc
IsaiahDcosta <93965458+IsaiahDcosta@users.noreply.github.com>
2023-09-13 09:16:05 +0530

    changed containerr right

--- frontend/src/components/ContainerRight/ContainerRight.js
+++ frontend/src/components/ContainerRight/ContainerRight.js
@@ -31,8 +31,8 @@ export default function ContainerRight({ data }) {
                   <Image
                     src={host + containerImage.attributes.url}
                     alt="container Image"
-                    width={1300}
-                    height={1300} />
+                    width={1000}
+                    height={1000} />
                 )}
 
               </div>
