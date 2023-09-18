e0e258392e4f6240c68c24d6c4bd91592d631c0c
carinadesouza <carinadesouza84@gmail.com>
2023-09-18 15:11:55 +0530

    updated changes

--- frontend/src/pages/services/index.js
+++ frontend/src/pages/services/index.js
@@ -1,8 +1,3 @@
-
-
-
-
-
 import Header from "@/components/Header/Header";
 import ContainerLeft from "@/components/ContainerLeft/ContainerLeft";
 import ContainerRight from "@/components/ContainerRight/ContainerRight";
